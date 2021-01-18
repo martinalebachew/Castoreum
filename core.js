2a9495d80ebdc1c80e54e8d68c3fe857f6c9ae23
iOSFractal <martin@pysch.org>
2021-01-18 15:56:40 +0200

    Answer all questions

--- core.js
+++ core.js
@@ -1,12 +1,41 @@
-var castoreum_COLLECTION = document.getElementsByClassName("activeQ")[0].getElementsByClassName("ansWrap")[0].children;
-var castoreum_ANSKEY = castoreum_COLLECTION[0].value;
-var castoreum_ANS;
+var castoreum_testLength = document.getElementsByClassName("qLength")[0].innerHTML;
 
-for (let i = 1; i <= 4; i++) {
-  if (castoreum_COLLECTION[i].getElementsByClassName("answer")[0].value == castoreum_ANSKEY) {
-    castoreum_ANS = castoreum_COLLECTION[i];
-    break;
+function castoreum_clickAnswer() {
+  var castoreum_COLLECTION = document.getElementsByClassName("activeQ")[0].getElementsByClassName("ansWrap")[0].children;
+  var castoreum_ANSKEY = castoreum_COLLECTION[0].value;
+  var castoreum_ANS;
+
+  for (let i = 1; i <= 4; i++) {
+    if (castoreum_COLLECTION[i].getElementsByClassName("answer")[0].value == castoreum_ANSKEY) {
+      castoreum_ANS = castoreum_COLLECTION[i];
+      break;
+    }
   }
+
+  castoreum_ANS.click();
 }
 
-castoreum_ANS.click();
+function castoreum_nextQuestion() {
+  var castoreum_nextButton = document.getElementsByClassName("testNav button nextButton nextQ")[0];
+  castoreum_nextButton.click();
+}
+
+function castoreum_endTest() {
+  var castoreum_endButton = document.getElementsByClassName("button disabled endButton")[0];
+  castoreum_endButton.click();
+}
+
+function castoreum_questionTimeout() {
+  castoreum_clickAnswer();
+  castoreum_nextQuestion();
+}
+
+var castoreum_last = false;
+
+while (!castoreum_last) {
+  var castoreum_QNUM = document.getElementsByClassName("activeQcnt")[0].innerHTML;
+  castoreum_last = (parseInt(castoreum_QNUM, 10) == parseInt(castoreum_testLength, 10));
+  castoreum_questionTimeout();
+}
+
+castoreum_endTest();
