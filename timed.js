6a27f288ad4b3f3cf13c7007c2d6b94872a4308e
iOSFractal <martin@pysch.org>
2021-01-19 12:37:59 +0200

    Site Update

--- timed.js
+++ timed.js
@@ -1,3 +1,4 @@
+const castoreum_sleep = (delay) => new Promise((resolve) => setTimeout(resolve, delay));
 var castoreum_testLength = document.getElementsByClassName("qLength")[0].innerHTML;
 
 function castoreum_clickAnswer() {
@@ -31,8 +32,11 @@ var castoreum_last = false;
 while (!castoreum_last) {
   var castoreum_QNUM = document.getElementsByClassName("activeQcnt")[0].innerHTML;
   castoreum_last = (parseInt(castoreum_QNUM, 10) == parseInt(castoreum_testLength, 10));
+  await castoreum_sleep(7000 + Math.random() * 1000);
   castoreum_clickAnswer();
+  await castoreum_sleep(2000 + Math.random() * 1000);
   castoreum_nextQuestion();
 }
 
+await sleep(3000 + Math.random() * 1000);
 castoreum_endTest();
